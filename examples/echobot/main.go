1a039a9f0273e932d28672dac5c84c9190ca4f08
AshokShau <114943948+AshokShau@users.noreply.github.com>
2026-01-29 23:52:55 +0530

    add dispatcher things

--- examples/echobot/main.go
+++ examples/echobot/main.go
@@ -3,70 +3,55 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 	"runtime"
-	"syscall"
 
 	"github.com/AshokShau/gotdbot"
+	"github.com/AshokShau/gotdbot/ext"
+	"github.com/AshokShau/gotdbot/ext/handlers"
+	"github.com/AshokShau/gotdbot/ext/handlers/filters"
 )
 
 func main() {
 	apiID := int32(6)
-	apiHash := ""
-	botToken := "BOT_TOKEN"
+	apiHash := "API_HASH"
+	botToken := "YOUR_BOT_TOKEN"
 
 	bot := gotdbot.NewClient(apiID, apiHash, botToken, &gotdbot.ClientConfig{LibraryPath: "./libtdjson.so.1.8.60"})
-	gotdbot.SetTdlibLogVerbosityLevel(2) // 1-5
+	gotdbot.SetTdlibLogVerbosityLevel(2)
 
-	bot.OnUpdateAuthorizationState(func(c *gotdbot.Client, u *gotdbot.UpdateAuthorizationState) error {
-		if u.AuthorizationState.Type() == "authorizationStateReady" {
-			me, err := c.GetMe()
-			if err != nil {
-				log.Fatalf("Failed to get me: %v", err)
-				return nil
-			}
-			fmt.Printf("Logged in as: %s %s (%d)\n", me.FirstName, me.LastName, me.Id)
+	dispatcher := ext.NewDispatcher(bot)
+
+	dispatcher.AddHandler(handlers.NewCommand("start", func(ctx *ext.Context) error {
+		_, err := ctx.Reply("Hello! I am an echo bot powered by gotdbot.", nil)
+		return err
+	}))
+
+	dispatcher.AddHandler(handlers.NewCommand("go", func(ctx *ext.Context) error {
+		reply := fmt.Sprintf("Hello! There are currently %d goroutines running.", runtime.NumGoroutine())
+		_, err := ctx.Reply(reply, nil)
+		return err
+	}))
+
+	dispatcher.AddHandler(handlers.NewMessage(filters.Text.And(filters.Incoming), func(ctx *ext.Context) error {
+		_, err := ctx.Client.ForwardMessages(ctx.EffectiveChatId, ctx.EffectiveChatId, []int64{ctx.EffectiveMessage.Id}, true, false, nil)
+		return err
+	}))
+
+	dispatcher.Start()
+
+	log.Println("Starting bot...")
+	if err := bot.Start(); err != nil {
+		log.Fatalf("Failed to start bot: %v", err)
+	}
+
+	me := bot.Me()
+	if me != nil {
+		username := ""
+		if me.Usernames != nil && len(me.Usernames.ActiveUsernames) > 0 {
+			username = me.Usernames.ActiveUsernames[0]
 		}
-		return nil
-	}, nil, 0)
+		log.Printf("Logged in as @%s (%d)", username, me.Id)
+	}
 
-	bot.OnUpdateNewMessage(func(c *gotdbot.Client, u *gotdbot.UpdateNewMessage) error {
-		msg := u.Message
-		if msg.Content.MessageText != nil && msg.Content.MessageText.Text != nil && msg.Content.MessageText.Text.Text == "/go" {
-			reply := fmt.Sprintf("Hello! There are currently %d goroutines running.", runtime.NumGoroutine())
-			_, err := c.SendMessage(msg.ChatId, &gotdbot.InputMessageContent{
-				InputMessageText: &gotdbot.InputMessageText{
-					Text: &gotdbot.FormattedText{
-						Text: reply,
-					},
-				},
-			}, nil)
-			if err != nil {
-				return err
-			}
-			return nil
-
-		}
-
-		_, err := c.ForwardMessages(msg.ChatId, msg.ChatId, []int64{msg.Id}, true, false, nil)
-		if err != nil {
-			return err
-		}
-		return nil
-	}, nil, 0)
-
-	bot.OnUpdateNewCallbackQuery(func(c *gotdbot.Client, msg *gotdbot.UpdateNewCallbackQuery) error {
-		fmt.Printf("Callback query received: %s\n", msg.Payload.CallbackQueryPayloadData)
-		return nil
-	}, nil, 0)
-
-	bot.Run()
-
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
-	<-sig
-
-	fmt.Println("Stopping...")
-	bot.Stop()
+	bot.Idle()
 }
